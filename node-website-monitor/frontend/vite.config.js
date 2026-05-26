8109fea4e1ec0ff7565c22013f01c0ac4bb6ea89
Tabitha <yourgithubemail@example.com>
2026-05-26 17:02:38 +0530

    Fixed frontend output folder

--- node-website-monitor/frontend/vite.config.js
+++ node-website-monitor/frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../backend/src/public',
+    outDir: 'build',
     emptyOutDir: true
   }
 });
